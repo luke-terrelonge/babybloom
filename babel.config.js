7d80111cc3f8d91e58cf3e413ebc86ccba84682b
luke-terrelonge <luketerrelonge@msn.com>
2026-07-20 09:35:04 +0100

    Sign Up Baby Information

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-      'nativewind/babel',
-    ],
-=======
 ﻿module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
->>>>>>> 5c824ba4c24a293e6a54f6b1592106d71249496c
+    presets: [
+     ['babel-preset-expo', { jsxImportSource: 'nativewind'}],
+    'nativewind/babel',
+  ],
   };
 };
